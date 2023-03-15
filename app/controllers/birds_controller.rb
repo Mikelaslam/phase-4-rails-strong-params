8cc094dcc124ed22b1fa2236a5f4d80aaa00570d
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-15 05:11:02 +0300

    Completed task

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,8 +8,8 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
+    bird = Bird.create(params)
+  render json: bird, status: :created
   end
 
   # GET /birds/:id
